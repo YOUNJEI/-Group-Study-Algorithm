a3000aae80754a84031d0766da7d100564d620b4
YOUNJEI <kyj951211@gmail.com>
2022-03-03 14:05:38 +0900

    7주차 윤제 rename & 파일 실수

--- 7주차 - 그리디/윤제/[1946] 신입사원/main.cpp
+++ 7주차 - 그리디/윤제/[1946] 신입사원/main.cpp
@@ -2,77 +2,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct s_person
+int solve(int *arr, int person_num)
 {
-    int docu_eval;
-    int meet_eval;
-}t_person;
+    int count = 0;
+    int target = arr[0];
 
-void solve(t_person **arr, int test_case_num, int *test_case_person_num)
-{
-    int *max_recruit = NULL;
-    int flag;
-    
-    max_recruit = (int*)malloc(sizeof(int) * test_case_num);
-    if (max_recruit == NULL)
-        return;
-    for (int i = 0; i < test_case_num; i++)
-        max_recruit[i] = 0;
-    for (int _loop = 0; _loop < test_case_num; _loop++)
+    for (int i = 0; i < person_num; i++)
     {
-        for (int i = 0; i < test_case_person_num[_loop]; i++)
+        if (arr[i] <= target)
         {
-            flag = 1;
-            for (int j = 0; j < test_case_person_num[_loop]; j++)
-            {
-                if (arr[_loop][i].docu_eval == 1 || arr[_loop][i].meet_eval == 1)
-                    break ;
-                if ((arr[_loop][i].docu_eval == test_case_person_num[_loop] || arr[_loop][i].meet_eval == test_case_person_num[_loop])
-                    && arr[_loop][i].docu_eval * arr[_loop][i].meet_eval > test_case_person_num[_loop])
-                {
-                    flag = 0;
-                    break ;
-                }
-                if (arr[_loop][i].docu_eval > arr[_loop][j].docu_eval
-                    && arr[_loop][i].meet_eval > arr[_loop][j].meet_eval)
-                    {
-                        flag = 0;
-                        break ;
-                    }
-            }
-            if (flag)
-                max_recruit[_loop]++;
+            target = arr[i];
+            count++;
         }
     }
-    for (int i = 0; i < test_case_num; i++)
-        printf("%d\n", max_recruit[i]);
+    return (count);
 }
 
 int main(void)
 {
     int test_case_num;
-    int *test_case_person_num = NULL;
-    t_person **arr = NULL;
+    int person_num;
+    int *arr = NULL;
+    int *answer = NULL;
+    int x, y;
 
     scanf("%d", &test_case_num);
-    arr = (t_person **)malloc(sizeof(t_person *) * test_case_num);
-    test_case_person_num = (int *)malloc(sizeof(int) * test_case_num);
-    if (arr == NULL || test_case_person_num == NULL)
+    answer = (int *)malloc(sizeof(int) * test_case_num);
+    if (answer == NULL)
         return (-1);
-    for(int i = 0; i < test_case_num; i++)
+    for (int i = 0; i < test_case_num; i++)
     {
-        scanf("%d", &test_case_person_num[i]);
-        arr[i] = (t_person *)malloc(sizeof(t_person) * test_case_person_num[i]);
-        if (arr[i] == NULL)
+        scanf("%d", &person_num);
+        arr = (int *)malloc(sizeof(int) * person_num);
+        if (arr == NULL)
             return (-1);
-        for(int j = 0; j < test_case_person_num[i]; j++)
+        for (int j = 0; j < person_num; j++)
         {
-            scanf("%d %d", &arr[i][j].docu_eval, &arr[i][j].meet_eval);
+            scanf("%d %d", &x, &y);
+            arr[x - 1] = y - 1;
         }
+        answer[i] = solve(arr, person_num);
+        free(arr);
     }
-    solve(arr, test_case_num, test_case_person_num);
-    for(int i = 0; i < test_case_num; i++)
-        free(arr[i]);
-    free(arr);
+    for (int i = 0; i < test_case_num; i++)
+        printf("%d\n", answer[i]);
+    free(answer);
     return (0);
 }
